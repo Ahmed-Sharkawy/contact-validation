761f0a672c8f833658e784c6606712359d79ce43
Ahmed-Sharkawy <ahmedmaher0110@gmail.com>
2021-10-10 07:10:53 +0200

    php_2.1

--- index.php
+++ index.php
@@ -1,70 +1,75 @@
 <?php
 // require "index.html";
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $Message = $_POST['Message'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $Message = $_POST['Message'];
 
-        $formEroores = [];
+    $formEroores = [];
 
-        if (strlen($username) == 0) {
-            $formEroores[] = "A field cannot be left username blank";
-        }
-        if (strlen($email) == 0) {
-            $formEroores[] = "A field cannot be left email blank";
-        }
-        if (strlen($phone) == 0) {
-            $formEroores[] = "A field cannot be left phone blank";
-        }
-        if (strlen($Message) == 0) {
-            $formEroores[] = "A field cannot be left Message blank";
-        }
+    if (strlen($username) == 0) {
+        $formEroores[] = "A field cannot be left <strong>username!</strong> blank";
     }
+    if (strlen($email) == 0) {
+        $formEroores[] = "A field cannot be left <strong>email!</strong> blank";
+    }
+    if (strlen($phone) == 0) {
+        $formEroores[] = "A field cannot be left <strong>phone!</strong> blank";
+    }
+    if (strlen($Message) == 0) {
+        $formEroores[] = "A field cannot be left <strong>Message!</strong> blank";
+    }
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <title>Document</title>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/all.min.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
+<head>
+    <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
-    <body>
+<body>
 
-        <!-- start Form -->
+    <!-- start Form -->
 
-        <section class="container">
-            <h1 class="text-center">Contact Me</h1>
-            <div class="formEroores">
-                <?php
-                    if (isset($formEroores)) {
-                        foreach ($formEroores as $Eroor) {
-                            echo $Eroor . "<br>";
-                        };
+    <section class="container">
+        <h1 class="text-center">Contact Me</h1>
+
+        <form action="index.php" method="POST">
+            <?php if (!empty($formEroores)) { ?>
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <?php
+                    foreach ($formEroores as $Eroor) {
+                        echo $Eroor . "<br>";
                     };
-                ?>
-            </div>
-            <form action="index.php" method="POST">
-                <input class="form-control" type="text" name="username" placeholder="username">
-                <input class="form-control" type="email" name="email" placeholder="email">
-                <input class="form-control" type="text" name="phone" placeholder="phone">
-                <textarea class="form-control" name="Message" placeholder="Your Message"></textarea>
-                <input class="btn btn-success btn-block" type="submit" value="send Message">
-            </form>
-        </section>
+                    ?>
+                </div>
+            <?php } ?>
+            <input class="form-control" type="text" name="username" placeholder="username">
+            <input class="form-control" type="email" name="email" placeholder="email">
+            <input class="form-control" type="text" name="phone" placeholder="phone">
+            <textarea class="form-control" name="Message" placeholder="Your Message"></textarea>
+            <input class="btn btn-success btn-block" type="submit" value="send Message">
+        </form>
+    </section>
 
-        <!-- End Form -->
-        <script src="js/jquery-3.6.0.min.js"></script>
-        <!-- <script src="js/bootstrap.min.js"></script> -->
-    
-    </body>
+    <!-- End Form -->
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
